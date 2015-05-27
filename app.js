f7645cf9f0e85ec019969a988f9dc98e85e9edd1
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-05-27 03:48:15 +0100

    Why an I still playing with this thing?

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var fs = require('fs'),
 mm = require('musicmetadata'),
 walk = require('walkdir'),
 path = require('path'),
-jf = require('jsonfile'),
 util = require('util'),
 mongoose = require('mongoose');
 
@@ -35,10 +34,10 @@ db.once('open', function (callback) {
 	var Album = mongoose.model('Album', albumSchema, 'Albums');
 	var Song = mongoose.model('Song', songSchema, 'Songs');	
 	
-	var musicPath = "music/";
+	var musicPath = "./music/The Flaming Lips";
 
 	var emitter = walk(musicPath),
-	songList = [];
+	albumCheck = [];
 
 	emitter.on('file',function(filename,stat){
 		if (path.extname(filename) == '.m4a' || path.extname(filename) == '.mp4' ) {
@@ -46,58 +45,49 @@ db.once('open', function (callback) {
 				if (err) throw err;
 				delete metadata.picture;
 				metadata.path = path.normalize(filename);
-				var parsed = path.parse(filename);
-				metadata.cover = parsed.dir + "/cover.jpg";
-				songList.push(metadata);
+				metadata.cover = path.dirname(filename) + "/cover.jpg";
+
+				var newSong = new Song({
+					songTitle: metadata.title,
+					songArtist: metadata.artist,
+					albumArtist: metadata.albumartist,
+					album: metadata.album,
+					year: metadata.year,
+					track: metadata.track,
+					genre: metadata.genre,
+					duration: metadata.duration,
+					path: metadata.path,
+					artPath: metadata.cover
+				});
+
+				newSong.save();
+				console.log(metadata.title);
+				
+				var newAlbum = new Album({
+					album: metadata.album,
+					albumArtist: metadata.albumartist,
+					year: metadata.year,
+					genre: metadata.genre,
+					artPath: metadata.cover
+				});
+
+				albumSchema.pre('save',function(next){
+					var self = this;
+					Album.find({album:metadata.album}, function(err, docs){
+						if (!docs.length){
+							newAlbum.save();
+							console.log("============= Saved album: " + metadata.album);
+							next();
+						} else {
+							console.log("Album Exists: " + metadata.album);
+							next();
+						}
+					})
+				})
 			});
 		};
 	});
 
-	emitter.on('end', function(){
-		console.log("Scan ended, naively waiting 5 secs");
 
-		setTimeout(function(){
-			console.log("Here's some stuff");
-			var albumCheck = [];
-
-			songList.forEach(function(value, index, array){
-
-				var newSong = new Song({
-					songTitle: value.title,
-					songArtist: value.artist,
-					albumArtist: value.albumartist,
-					album: value.album,
-					year: value.year,
-					track: value.track,
-					genre: value.genre,
-					duration: value.duration,
-					path: value.path,
-					artPath: value.cover
-				});
-
-				if (albumCheck.indexOf(value.album) < 0) {
-					
-					albumCheck.push(value.album);
-
-					var newAlbum = new Album({
-						album: value.album,
-						albumArtist: value.albumartist,
-						year: value.year,
-						genre: value.genre,
-						artPath: value.cover
-					});
-
-					newAlbum.save();
-					
-				};
-
-				newSong.save();
-
-				console.log(newAlbum);
-			})
-
-		}, 5000)
-
-	});
 });
 
