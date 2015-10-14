f5ba877ba199827b821b4949c6e34fda25a841cb
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-10-14 19:17:07 +0100

    Updated, more sensible versions

--- album_scan.js
+++ album_scan.js
@@ -2,96 +2,70 @@ var fs = require('fs'),
 mm = require('musicmetadata'),
 walk = require('walkdir'),
 path = require('path'),
-util = require('util'),
 mongoose = require('mongoose'),
 async = require('async');
-
+var colors = require('colors');
 
 mongoose.connect('mongodb://localhost/Doppler');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function (callback) {
-	var albumSchema = mongoose.Schema({
-		albumArtist: [String],
-		album: String,
-		year: String,
-		genre: Array,
-		artPath: String
-	});
-
-	var Album = mongoose.model('Album', albumSchema, 'Albums');
-	
-	var musicPath = "../music/";
-
-	var emitter = walk(musicPath),
-	albumAr = [];
-
-	emitter.on('file',function(filename,stat){
-		if (path.extname(filename) == '.m4a' || path.extname(filename) == '.mp4' ) {
-			var parser = mm(fs.createReadStream(filename),{duration: true}, function (err, metadata) {
-				if (err) throw err;
-				delete metadata.picture;
-				metadata.path = path.relative('/home/',filename);
-				metadata.year = metadata.year.slice(0,4);
-				metadata.cover = path.dirname(metadata.path) + "/cover.jpg";
-
-				var newAlbum = new Album({
-					album: metadata.album,
-					albumArtist: metadata.albumartist,
-					year: metadata.year,
-					genre: metadata.genre,
-					artPath: metadata.cover
-				});
-
-				albumAr.push(newAlbum);
-				
-			});
-		};
-	});
-
-	emitter.on('end', function(){
-
-		
-		console.log("scan ended");
-
-		setTimeout(function(){
-			
-			console.log(albumAr.length);
-			
-			// albumAr.forEach(function(element, index, array){
-				async.eachSeries(albumAr, function(element, callback){
-					
-					Album.findOne({album: element.album}, function(err, docs){
-
-						if (err){
-							console.log("Mongo error: " + err);
-							return false;
-						}
-						if (!docs){
-							console.log("============= Saved album: " + element.album);
-							element.save(function(err){
-								if (err) console.log(err);
-								callback();
-							});
-						} else {
-							console.log("Album Exists: " + element.album);
-							callback();
-						}
-
-					});
-
-				}, function(args){
-					// body
-				})
-
-			// });
-		}, 2000)
-	})	
-
-
-	
-
-
+var albumSchema = mongoose.Schema({
+	albumArtist: [String],
+	album: String,
+	year: String,
+	genre: Array,
+	artPath: String
 });
 
+var Album = mongoose.model('Album', albumSchema, 'Albums');
+var musicPath = process.argv[2] || '../music';
+var emitter = walk(musicPath),
+albumAr = [];
+
+emitter.on('file',function(filename,stat){
+	if (path.extname(filename) == '.m4a' || path.extname(filename) == '.mp4' ) {
+		mm(fs.createReadStream(filename),{duration: true}, function (err, metadata) {
+			if (err) throw err;
+			delete metadata.picture;
+			metadata.path = path.relative(musicPath, filename);
+			metadata.cover = path.dirname(metadata.path) + "/cover.jpg";
+
+			var newAlbum = new Album({
+				album: metadata.album,
+				albumArtist: metadata.albumartist,
+				year: metadata.year.slice(0,4),
+				genre: metadata.genre,
+				artPath: metadata.cover
+			});
+
+			albumAr.push(newAlbum);	
+		});
+	};
+});
+
+emitter.on('end', function(){
+	setTimeout(function(){
+		console.log(albumAr.length);
+		async.eachSeries(albumAr, function(element, callback){
+			
+			Album.findOne({album: element.album}, function(err, docs){
+
+				if (err){
+					console.log("Mongo error: " + err);
+					return false;
+				}
+				if (!docs){
+					console.log("Saved album: ".green + element.album);
+					element.save(function(err){
+						if (err) console.log(err);
+						callback();
+					});
+				} else {
+					console.log("Album Exists: ".red + element.album);
+					callback();
+				}
+			});
+		});
+	}, 2000)
+});
--- song_scan.js
+++ song_scan.js
@@ -2,75 +2,69 @@ var fs = require('fs'),
 mm = require('musicmetadata'),
 walk = require('walkdir'),
 path = require('path'),
-util = require('util'),
 mongoose = require('mongoose');
+var colors = require('colors');
 
 mongoose.connect('mongodb://localhost/Doppler');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function (callback) {
+var songSchema = mongoose.Schema({
+	songTitle: String,
+	songArtist: Array,
+	albumArtist: Array,
+	album: String,
+	year: String,
+	track: Object,
+	disk: Object,
+	genre: Array,
+	path: String,
+	artPath: String
+})
 
-	var songSchema = mongoose.Schema({
-		songTitle: String,
-		songArtist: [String],
-		albumArtist: [String],
-		album: String,
-		year: String,
-		track: Number,
-		genre: Array,
-		duration: Number,
-		path: String,
-		artPath: String
-	})
+var Song = mongoose.model('Song', songSchema, 'Songs');	
+var musicPath = process.argv[2] || '../music';
+var emitter = walk(musicPath);
 
-	var Song = mongoose.model('Song', songSchema, 'Songs');	
-	
-	var musicPath = "../music/";
+emitter.on('file',function(filename, stat){
+	if (path.extname(filename) == '.m4a' || path.extname(filename) == '.mp4' ) {
 
-	var emitter = walk(musicPath),
-	albumCheck = [];
+		mm(fs.createReadStream(filename), function (err, metadata) {
+			if (err) throw err;
+			delete metadata.picture;
+			metadata.path = path.relative(musicPath,filename);
+			metadata.cover = path.dirname(metadata.path) + "/cover.jpg";
 
-	emitter.on('file',function(filename,stat){
-		if (path.extname(filename) == '.m4a' || path.extname(filename) == '.mp4' ) {
-			var parser = mm(fs.createReadStream(filename),{duration: true}, function (err, metadata) {
-				if (err) throw err;
-				delete metadata.picture;
-				metadata.path = path.relative('/home/',filename);
-				metadata.year = metadata.year.slice(0,4);
-				metadata.cover = path.dirname(metadata.path) + "/cover.jpg";
-				metadata.track = metadata.track.no;
-
-				var newSong = new Song({
-					songTitle: metadata.title,
-					songArtist: metadata.artist,
-					albumArtist: metadata.albumartist,
-					album: metadata.album,
-					year: metadata.year,
-					track: metadata.track,
-					genre: metadata.genre,
-					duration: metadata.duration,
-					path: metadata.path,
-					artPath: metadata.cover
-				});
-
-				Song.findOne({songTitle: metadata.title}, function(err, docs){
-					if (err){
-						console.log("Mongo error: " + err)
-						return false;
-					}
-					if (!docs){
-						console.log("++++ Saved song:" + metadata.title);
-						newSong.save();
-					} else {
-						console.log("Song exists: " + metadata.title);
-					}
-				});
-				
+			var newSong = new Song({
+				songTitle: metadata.title,
+				songArtist: metadata.artist,
+				albumArtist: metadata.albumartist,
+				album: metadata.album,
+				year: metadata.year.slice(0,4),
+				track: metadata.track,
+				disk: metadata.disk,
+				genre: metadata.genre,
+				path: metadata.path,
+				artPath: metadata.cover
 			});
-		};
-	});
 
-
+			Song.findOne({songTitle: metadata.title}, function(err, docs){
+				if (err){
+					console.log("Mongo error: " + err)
+					return false;
+				}
+				if (!docs){
+					console.log("Saved song:".green + metadata.title);
+					newSong.save();
+				} else {
+					console.log("Song exists: ".red + metadata.title);
+				}
+			});
+		});
+	};
+});
+emitter.on('error', function(path, error) {
+	console.log(error);
+	process.exit(1);
 });
 
